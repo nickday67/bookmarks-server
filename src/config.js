32d480fbbe5b019f75a17e9cf31dd808ae82934a
Nick Day <thenickday@icloud.com>
2020-01-29 17:08:40 -0500

    added missing code

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT : process.env.PORT || 8000,
   NODE_ENV : process.env.NODE_ENV || 'development',
-  API_TOKEN : 'fd3fe1bb-c4ec-40ef-ae4a-686502854870'
+  API_TOKEN : process.env.API_TOKEN || 'fd3fe1bb-c4ec-40ef-ae4a-686502854870'
 }
